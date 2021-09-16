313177b84157f00efe2411867a16a58bf5c60670
Hi-Bean <halloqls@naver.com>
2021-09-16 17:47:04 +0900

    20210916 _ C#_Notepad

---
+++ C#/NotePad/NotePad/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileSavAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFilePrint = new System.Windows.Forms.ToolStripMenuItem();
+            this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewLower = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewUpper = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewHex = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewBack = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.mnuEditTest = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(800, 30);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileNew,
+            this.toolStripMenuItem1,
+            this.mnuFileOpen,
+            this.mnuFileSave,
+            this.mnuFileSavAs,
+            this.toolStripMenuItem2,
+            this.mnuFilePrint,
+            this.printPreviewToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuFileNew
+            // 
+            this.mnuFileNew.Name = "mnuFileNew";
+            this.mnuFileNew.Size = new System.Drawing.Size(224, 26);
+            this.mnuFileNew.Text = "New";
+            this.mnuFileNew.Click += new System.EventHandler(this.mnuFileNew_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.Size = new System.Drawing.Size(224, 26);
+            this.mnuFileOpen.Text = "Open";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // mnuFileSave
+            // 
+            this.mnuFileSave.Name = "mnuFileSave";
+            this.mnuFileSave.Size = new System.Drawing.Size(224, 26);
+            this.mnuFileSave.Text = "Save";
+            this.mnuFileSave.Click += new System.EventHandler(this.mnuFileSave_Click);
+            // 
+            // mnuFileSavAs
+            // 
+            this.mnuFileSavAs.Name = "mnuFileSavAs";
+            this.mnuFileSavAs.Size = new System.Drawing.Size(224, 26);
+            this.mnuFileSavAs.Text = "Save As ...";
+            this.mnuFileSavAs.Click += new System.EventHandler(this.mnuFileSavAs_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(221, 6);
+            // 
+            // mnuFilePrint
+            // 
+            this.mnuFilePrint.Name = "mnuFilePrint";
+            this.mnuFilePrint.Size = new System.Drawing.Size(224, 26);
+            this.mnuFilePrint.Text = "Print";
+            this.mnuFilePrint.Click += new System.EventHandler(this.mnuFilePrint_Click);
+            // 
+            // printPreviewToolStripMenuItem
+            // 
+            this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.printPreviewToolStripMenuItem.Text = "Print Preview ...";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(221, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuEditTest});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 26);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuViewLower,
+            this.mnuViewUpper,
+            this.mnuViewHex,
+            this.mnuViewBack});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(56, 26);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // mnuViewLower
+            // 
+            this.mnuViewLower.Name = "mnuViewLower";
+            this.mnuViewLower.Size = new System.Drawing.Size(180, 26);
+            this.mnuViewLower.Text = "lower case ...";
+            this.mnuViewLower.Click += new System.EventHandler(this.mnuViewLower_Click);
+            // 
+            // mnuViewUpper
+            // 
+            this.mnuViewUpper.Name = "mnuViewUpper";
+            this.mnuViewUpper.Size = new System.Drawing.Size(180, 26);
+            this.mnuViewUpper.Text = "upper case ...";
+            this.mnuViewUpper.Click += new System.EventHandler(this.mnuViewUpper_Click);
+            // 
+            // mnuViewHex
+            // 
+            this.mnuViewHex.Name = "mnuViewHex";
+            this.mnuViewHex.Size = new System.Drawing.Size(180, 26);
+            this.mnuViewHex.Text = "hex mode";
+            this.mnuViewHex.Click += new System.EventHandler(this.mnuViewHex_Click);
+            // 
+            // mnuViewBack
+            // 
+            this.mnuViewBack.Name = "mnuViewBack";
+            this.mnuViewBack.Size = new System.Drawing.Size(180, 26);
+            this.mnuViewBack.Text = "return back";
+            this.mnuViewBack.Click += new System.EventHandler(this.mnuViewBack_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // tbNote
+            // 
+            this.tbNote.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNote.Location = new System.Drawing.Point(0, 29);
+            this.tbNote.Multiline = true;
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(799, 488);
+            this.tbNote.TabIndex = 1;
+            this.tbNote.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbNote_KeyDown);
+            // 
+            // mnuEditTest
+            // 
+            this.mnuEditTest.Name = "mnuEditTest";
+            this.mnuEditTest.Size = new System.Drawing.Size(224, 26);
+            this.mnuEditTest.Text = "Test";
+            this.mnuEditTest.Click += new System.EventHandler(this.mnuEditTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "Form1";
+            this.Text = "HB\'s Notepad v.1.0";
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileNew;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileSavAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem mnuFilePrint;
+        private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewLower;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewUpper;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewHex;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewBack;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem mnuEditTest;
+    }
+}
+
---
+++ C#/NotePad/NotePad/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        public string filePath;     // 파일 경로
+        public string filename;     // 파일명
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string strOrg = "";
+        int viewState = 0;        // 0:Normal    1:Lower    2:Upper    3:Hexa 
+        private void mnuViewLower_Click(object sender, EventArgs e)
+        {
+            if(viewState != 1)
+            {
+                if (strOrg == "") strOrg = tbNote.Text;
+                tbNote.Text = strOrg.ToLower();
+                tbNote.ReadOnly = true;
+                viewState = 1;
+            }
+        }
+
+        private void mnuViewUpper_Click(object sender, EventArgs e)
+        {
+            if (viewState != 2)
+            {
+                if (strOrg == "") strOrg = tbNote.Text;
+                tbNote.Text = strOrg.ToUpper();
+                tbNote.ReadOnly = true;
+                viewState = 2;
+            }
+        }
+
+        private void mnuViewHex_Click(object sender, EventArgs e)
+        {
+            if (viewState != 3)
+            {
+                if (strOrg == "") strOrg = tbNote.Text;
+                tbNote.Text = "";
+
+                string s1;
+                char[] chr = strOrg.ToCharArray();
+                byte[] bArr = Encoding.Default.GetBytes(chr);
+
+                for(int i=0; i<bArr.GetLength(0); i++)
+                {
+                    //s1 = string.Format(" {0:X2}", bArr[i]);   // printf(" %x ", n);
+                    s1 = $" {bArr[i]:X2}";
+                    if (i % 16 == 15) s1 += "\r\n";
+                    tbNote.Text += s1;
+                }
+
+                //////위의 결과와 같음 (foreach문 사용한 경우)
+                ////tbMemo.Text += "\r\n==========================================\r\n";
+                ////int count = 0;
+                //////  for (int i = 0; i < strOrg.Length; i++)
+                ////foreach (byte c in bArr)
+                ////{
+                ////    s1 = $" {c:X2}";              //   string.Format(" {0:d} ", chr[i]);   // printf(" %x ", n);
+                ////    if (count++ % 16 == 15) s1 += "\r\n";
+                ////    tbMemo.Text += s1;
+                ////    //count++;
+                ////}
+
+                tbNote.ReadOnly = true;
+                viewState = 3;
+            }
+            }
+
+        private void mnuViewBack_Click(object sender, EventArgs e)
+        {
+            if(strOrg != "")
+            {
+                tbNote.Text = strOrg;
+                strOrg = "";
+                tbNote.ReadOnly = false;
+                viewState = 0;
+            }
+        }
+
+        private void tbNote_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                mnuViewBack_Click(sender, e);
+        }
+
+        //Menu - File - NEW
+        private void mnuFileNew_Click(object sender, EventArgs e)
+        {
+            string str = Application.ExecutablePath;
+            Process.Start(str);
+
+            //// 외부프로그램 종료
+            //foreach(Process p in Process.GetProcesses())
+            //{
+            //    if (p.ProcessName.StartsWith("myNotepad"))
+            //        p.Kill();
+            //}
+        }
+
+        //Menu - File - Open
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Text file(.txt)|*.txt|C#(.cs)|*.cs|All files|*.*";
+            openFileDialog.FilterIndex = 3;
+            //tbMemo.Text += openFileDialog.FileName;      // filename 불러오기
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 파일 경로 & 파일 경로에서 파일명 추출
+                filePath = openFileDialog.FileName;
+                filename = filePath.Substring(filePath.LastIndexOf("\\") + 1);
+                //filename = Path.GetFileName(filePath);
+
+                // streamreader 열어서 파일 불러오기
+                StreamReader sr = new StreamReader(openFileDialog.FileName);
+                tbNote.Text = sr.ReadToEnd();
+                sr.Close();     // stream 사용 후엔 항상 닫아주기!
+
+                // 메모장 창에 파일 이름 띄우기
+                Text = filename;
+            }
+        }
+
+        // Menu - File - Save
+        private void mnuFileSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "Text file(.txt)|*.txt|C#(.cs)|*.cs|All files|*.*";
+            saveFileDialog.FilterIndex = 1;
+
+            if (filename != "") // open으로 파일을 불러왔을 경우
+            {
+                // file경로는 open으로 불러온 경로
+                saveFileDialog.FileName = filePath;
+                StreamWriter sw = new StreamWriter(filePath);
+                sw.Write(tbNote.Text);
+                sw.Close();
+            }
+            else
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 경로와 파일명 재설정
+                    filePath = saveFileDialog.FileName;
+                    filename = Path.GetFileName(filePath);
+
+                    // 파일 저장
+                    StreamWriter sw = new StreamWriter(filePath);
+                    sw.Write(tbNote.Text);
+                    sw.Close();
+                }
+            }
+            // 창에 파일명 띄우기
+            Text = filename;
+        }
+
+        // Menu - File - Save As
+        private void mnuFileSavAs_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "Text file(.txt)|*.txt|C#(.cs)|*.cs|All files|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.FileName = filename;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 경로와 파일명 재설정
+                filePath = saveFileDialog.FileName;
+                filename = Path.GetFileName(filePath);
+
+                // 파일 저장
+                StreamWriter sw = new StreamWriter(filePath);
+                sw.Write(tbNote.Text);
+                sw.Close();
+
+                // 창에 파일명 띄우기
+                Text = filename;
+            }
+        }
+
+        // prototype    : string GetToken(int n, string str, char d);
+        // Argument     :
+        //      n       : n번째 item
+        //     str      : 문자열
+        //      d       : 구분자
+        // 설명         : 문자열 str에 있는 데이터를 구분자 d를 통해 필드를 구분
+        //                그 중 n 번째 데이터 반환
+        //              ex) GetToken(1, "a,b,c,d", ",")  ===>  "b"
+        //              ex2) GetToken(2, "app,bee,carrot,dark", ',')  ===>  "carrot"
+        public string GetToken(int n, string str, char d)
+        {
+            int count = 0; string result = "";
+            //char[] ch = str.ToCharArray();
+            for (int i = 0; i <= str.Length; i++)
+            {
+                if (str[i] != d) continue;
+                count++;
+                if(n == count)
+                {
+                    for(int j=i+1; str[j] != d; j++)
+                    {
+                        result += str[j];
+                    }
+                    break;
+                }
+            }
+            return result;
+        }
+
+        string GetToken_byLee(int n, string str, char d)
+        {
+            //// case 1
+            //int i, j, k, n1, n2;        // n1 : start, n2 : end
+            //for(i=j=k=n1=n2=0; i<str.Length; i++)
+            //{
+            //    if (str[i] == d) k++;
+            //    if (k == n) n1 = i;
+            //    if (k == n + 1) n2 = i;
+            //}
+            //if (n1 == 0) return "";
+            //if (n2 == 0) n2 = str.Length;
+            //return str.Substring(n1, n2 - n1);
+
+            // case 2
+            string[] sArr = str.Split(d);
+            if (n < sArr.Length) return sArr[n];
+            else return "";
+        }
+
+        private void mnuFilePrint_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void mnuEditTest_Click(object sender, EventArgs e)
+        {
+            tbNote.Text += GetToken(2, "app,bee,carrot,dark", ',') + "\r\n";
+            tbNote.Text += $"{GetToken_byLee(3, "apple,banana,cake,doll", ',')}";
+        }
+    }
+}
